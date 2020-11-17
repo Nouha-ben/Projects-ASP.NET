687d923ebb75cf039ef697356260301e53e7d1e0
Nouha Ben <nouhabenlarabi@gmail.com>
2020-11-17 20:27:19 +0100

    first commit

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {// 
+     // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
